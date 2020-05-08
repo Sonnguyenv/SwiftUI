90de449634ee01de01bbdce4041e36548f884899
sonnv <sonnv@rikkeisoft.com>
2020-05-08 17:45:58 +0700

    add list

--- BasisSwiftUI/BasisSwiftUI/ContentView.swift
+++ BasisSwiftUI/BasisSwiftUI/ContentView.swift
@@ -8,9 +8,57 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id: Int
+    
+    let name, price, imageName: String
+}
+
 struct ContentView: View {
+    
+    let users: [User] = [.init(id: 0, name: "Iphone", price: "1000 USD", imageName: "iphone"),
+                        .init(id: 1, name: "Ipad", price: "1000 USD", imageName: "ipad"),
+                        .init(id: 2, name: "Watch", price: "1000 USD", imageName: "watch"),
+                        .init(id: 3, name: "Macbook", price: "1000 USD", imageName: "macbook"),
+                        .init(id: 0, name: "Iphone", price: "1000 USD", imageName: "iphone"),
+                        .init(id: 1, name: "Ipad", price: "1000 USD", imageName: "ipad"),
+                        .init(id: 2, name: "Watch", price: "1000 USD", imageName: "watch"),
+                        .init(id: 3, name: "Macbook", price: "1000 USD", imageName: "macbook"),
+                        .init(id: 0, name: "Iphone", price: "1000 USD", imageName: "iphone"),
+                        .init(id: 1, name: "Ipad", price: "1000 USD", imageName: "ipad"),
+                        .init(id: 2, name: "Watch", price: "1000 USD", imageName: "watch"),
+                        .init(id: 3, name: "Macbook", price: "1000 USD", imageName: "macbook")
+        
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView() {
+            List {
+                ForEach(users, id: \.id) { user in
+                    UserView(user: user)
+                }
+            }
+            .navigationBarTitle(Text("Dynamic List"))
+        }
+    }
+}
+
+struct UserView: View {
+    let user: User
+
+    var body: some View {
+        HStack {
+            Image(user.imageName)
+                .resizable()
+                .frame(width: 70, height: 70)
+                .clipShape(Circle())
+                .clipped()
+            VStack(alignment: .leading) {
+                Text(user.name).font(.headline)
+                    .padding(.init(top: 5, leading: 5, bottom: 5, trailing: 5))
+                Text(user.price).font(.subheadline)
+            }.padding(.leading)
+        }.padding(.init(top: 5, leading: 5, bottom: 5, trailing: 5))
     }
 }
 
