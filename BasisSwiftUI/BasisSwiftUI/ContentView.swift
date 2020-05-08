9db3b4efe34814eaee7593237c1b8fcde8569a14
sonnv <sonnv@rikkeisoft.com>
2020-05-08 16:02:27 +0700

    create project

---
+++ BasisSwiftUI/BasisSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasisSwiftUI
+//
+//  Created by SonNV on 5/8/20.
+//  Copyright © 2020 SonNV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
